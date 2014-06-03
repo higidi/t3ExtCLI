cba1db982f2dbcfbffbfc175667abd56f9de30bf
Daniel Hürtgen <daniel@higidi.com>
2014-06-03 15:28:52 +0200

    [TASK] T3EextCLI app dispatcher file initialized

---
+++ run.php
@@ -0,0 +1,16 @@
+<?php
+
+// autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// define environment constants
+define('APP_NAME', 'T3ExtCLI');
+define('APP_VERSION', '1.0.0-dev');
+
+// create application
+$app = new \Cilex\Application(APP_NAME, APP_VERSION);
+
+// TODO do stuff here :-)
+
+// run application
+$app->run();
