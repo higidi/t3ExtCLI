3019941115dbd4b34dbf1cad513d0b26a1b226d3
Daniel Hürtgen <daniel@higidi.com>
2014-10-08 23:39:04 +0200

    [TASK] dummy phpunit testcase added

---
+++ tests/src/T3ExtCli/ApplicationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testNotImplemented()
+    {
+        $this->markTestSkipped('Tests needs to be written.');
+    }
+
+}
