3dfc28fc74833b6d4f957078d9bdfccd76f36348
Daniel Hürtgen <daniel@higidi.com>
2014-10-09 22:59:10 +0200

    [FEATURE] test boostrap file added

---
+++ tests/src/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Higidi/T3ExtCLI package.
+ *
+ * (c) Daniel Hürtgen <daniel@higidi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+error_reporting(E_ALL);
+
+if (function_exists('date_default_timezone_set') && function_exists('date_default_timezone_get')) {
+    date_default_timezone_set(@date_default_timezone_get());
+}
+
+$autoloader = require __DIR__.'/../../src/bootstrap.php';
