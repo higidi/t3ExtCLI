cec6cd9d01c7bdb7750a49cdcc62c925a92c55ab
Daniel Hürtgen <daniel@higidi.com>
2014-10-13 22:37:13 +0200

    [FEATURE] typo3 ter base command initialized

---
+++ src/T3ExtCli/Command/Ter/Command.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Higidi/T3ExtCLI package.
+ *
+ * (c) Daniel Hürtgen <daniel@higidi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace T3ExtCli\Command\Ter;
+
+use Cilex\Command\Command as BaseCommand;
+
+/**
+ * Typo3 base command.
+ *
+ * @author Daniel Hürtgen <daniel@higidi.com>
+ */
+class Command extends BaseCommand {
+}
