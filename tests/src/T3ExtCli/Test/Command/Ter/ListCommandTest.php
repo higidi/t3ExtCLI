52b86c044d4051838a77c550ebc6d4455c25905d
Daniel Hürtgen <daniel@higidi.com>
2014-10-14 17:41:22 +0200

    [FEATURE] basic test cases for list/upload/download commands added

---
+++ tests/src/T3ExtCli/Test/Command/Ter/ListCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Higidi/T3ExtCLI package.
+ *
+ * (c) Daniel Hürtgen <daniel@higidi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace T3ExtCli\Test\Command\Ter;
+
+class ListCommandTest extends \PHPUnit_Framework_TestCase {
+
+    public function testNotImplementedYet()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+}
