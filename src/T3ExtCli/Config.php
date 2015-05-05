503991e7a7414d3c8dd9b5c83da71d86adc99be7
Daniel Hürtgen <daniel@higidi.de>
2015-05-05 23:51:34 +0200

    [FEATURE] config class initialized

---
+++ src/T3ExtCli/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Higidi/T3ExtCLI package.
+ *
+ * (c) Daniel Hürtgen <daniel@higidi.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace T3ExtCli;
+
+/**
+ * The T3ExtCLI config class.
+ *
+ * @author Daniel Hürtgen <daniel@higidi.de>
+ *
+ * @api
+ */
+class Config
+{
+}
---
+++ tests/src/T3ExtCli/Test/ConfigTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Higidi/T3ExtCLI package.
+ *
+ * (c) Daniel Hürtgen <daniel@higidi.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace T3ExtCli\Test;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testNotImplementedYet()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+}
